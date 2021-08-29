48b2330f20aa7cde681013af7f8664c5b7ffdd1e
Daniel <danielselga@gmail.com>
2021-08-29 01:07:31 -0300

    chore: install typescript

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+
+// Blocking, sync way
+const textIn = fs.readFileSync('./txt/input.txt', 'UTF-8')
 
 console.log(textIn)
 
 const textOut = `This is what we know about the avocao: ${textIn}. \nCreated on ${Date.now()}`
 fs.writeFileSync('./txt/output.txt', textOut)
 console.log('File written')
+
+//  Non-block async way
+fs.readFile('./txt/start.txt', 'UTF-8')
