7b82396e9b284e43f3e16e8edf965b8d78d863a8
Daniel <danielselga@gmail.com>
2021-09-01 01:30:58 -0300

    feature: HTML Templating

--- index.ts
+++ index.ts
@@ -29,31 +29,56 @@ const url = require('url')
 // console.log('will read file')
 
 //// Server
-const data: string = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'UTF-8')
-const dataObj : object = JSON.parse(data) //Only will executed once, sync toplevel code.
+const replaceTemplate = (temp : any, product : any) => {
+  let output = temp.replaceAll('{%PRODUCTNAME%}', product.productName)
+  output = output.replaceAll('{%IMAGE%}', product.image)
+  output = output.replaceAll('{%PRICE%}', product.price)
+  output = output.replaceAll('{%FROM%}', product.from)
+  output = output.replaceAll('{%NUTRIENTS%}', product.nutrients)
+  output = output.replaceAll('{%QUANTITY%}', product.quantity)
+  output = output.replaceAll('{%DESCRIPTION%}', product.description)
+  output = output.replaceAll('{%ID%}', product.id)
 
+  if(!product.organic) {
+    output = output.replace(/{%NOTORGANIC%}/g, 'not-organic')
+  }
+
+  return output
+}
+
+const tempOverview: string = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'UTF-8')
+const tempCard: string = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'UTF-8')
+const tempProduct: string = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'UTF-8')
+const data: string = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'UTF-8')
+const dataObj : Array<object> = JSON.parse(data) //Only will executed once, sync toplevel code.
 
 const server = http.createServer((req : any, res : any) => {
 const pathName : string = req.url
 
+  //Overview Page
 if(pathName === '/' || pathName === '/overview') {
-
-  res.end('This is the OVERVIEW')
+  res.writeHead(200, {'Content-Type': 'text/html'})
   
+  const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+  const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+
+  res.end(output)
+  
+  // Product Page
 } else if (pathName === '/product') {
 
   res.end('This is the PRODUCT')
 
+  //API
 } else if (pathName === '/api') {
-
+  res.writeHead(200, {'Content-Type': 'applicaton/json'})
   res.end(data) // reading the data var witch contains the JSON string.
 
+  //Not Found
 } else {
 
-    res.writeHead(404, {
-        'Content-Type': 'text/html',
-        'my-own-header': 'hello-world'
-    })
+    res.writeHead(404, {'Content-Type': 'text/html', 'my-own-header': 'hello-world'})
+
     res.end('<h1>ERROR 404</h1>')
 }
 })
